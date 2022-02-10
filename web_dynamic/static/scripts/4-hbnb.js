26d6ac52d3ac8b80c9ccf45f0448cd7afd3dd0ff
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-10 16:51:55 -0500

    [UPD] - logic and smal adjustment

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -18,14 +18,18 @@ $(document).ready(function () {
         }
         $('#amenities_filter').text('')
         for (let i = 0; i < amenities.length; i++) {
-            if (i == 0)
-                $('#amenities_filter').append(amenities[i])
-            else
-                $('#amenities_filter').append(", " + amenities[i])
+            if ($('#amenities_filter')[0].textContent.length < 40) {
+                if (i == 0)
+                    $('#amenities_filter').append(amenities[i])
+                else
+                    $('#amenities_filter').append(", " + amenities[i])
+            } else {
+                $('#amenities_filter').append("...")
+                break;
+            }
         }
     });
-    $.get("http://localhost:5001/api/v1/status/", function(data)
-    {
+    $.get("http://localhost:5001/api/v1/status/", function (data) {
         if (data.status === "OK") {
             $("#api_status").css("background-color", "#ff545f");
             $("#api_status").addClass("available");
@@ -41,7 +45,7 @@ $.ajax({
     url: "http://localhost:5001/api/v1/places_search",
     data: JSON.stringify({}),
     contentType: 'application/json',
-    success: function(data){
+    success: function (data) {
         for (let i = 0; i < data.length; i++) {
             $("section.places").append($("<article id=" + data[i].id + "></article>"))
             $("section.places article#" + data[i].id).append($("<div class='title_box'></div>"))
@@ -53,53 +57,51 @@ $.ajax({
             $("section.places article#" + data[i].id + " div.information").append($("<div class='number_bathrooms'>" + data[i].number_bathrooms + " Bathroom" + (data[i].number_bathrooms != 1 ? 's' : '') + "</div>"))
             $("section.places article#" + data[i].id + "").append($("<div class='user'></div>"))
             $.get("http://localhost:5001/api/v1/users/" + data[i].user_id,
-            function(data_user)
-            {
-                $("section.places article#" + data[i].id + " div.user").append($("<b>Owner: </b>"))
-                $("section.places article#" + data[i].id + " div.user").append((data_user.first_name + " " + data_user.last_name))
-            });
+                function (data_user) {
+                    $("section.places article#" + data[i].id + " div.user").append($("<b>Owner: </b>"))
+                    $("section.places article#" + data[i].id + " div.user").append((data_user.first_name + " " + data_user.last_name))
+                });
             $("section.places article#" + data[i].id + "").append($("<div class='description'></div>"))
             $("section.places article#" + data[i].id + " div.description").append(data[i].description)
         }
     },
     dataType: 'json'
-  });
+});
 
-function search_places(){
-	console.log("busqueda exitosa")
+function search_places() {
+    console.log("busqueda exitosa")
     const dict = {
         "state": '',
-        "cities":'',
+        "cities": '',
         "amenities": amenities_id
     };
-	$("article").remove();
-	$.ajax({
-		type: "POST",
-		url: "http://localhost:5001/api/v1/places_search",
-		data: JSON.stringify(dict),
-		contentType: 'application/json',
-		success: function(data){
+    $("article").remove();
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:5001/api/v1/places_search",
+        data: JSON.stringify(dict),
+        contentType: 'application/json',
+        success: function (data) {
             console.log(data);
-		    for (let i = 0; i < data.length; i++) {
-			$("section.places").append($("<article id=" + data[i].id + "></article>"))
-			$("section.places article#" + data[i].id).append($("<div class='title_box'></div>"))
-			$("section.places article#" + data[i].id + " div.title_box").append($("<h2>" + data[i].name + "</h2>"))
-			$("section.places article#" + data[i].id + " div.title_box").append($("<div class='price_by_night'>$" + data[i].price_by_night + "</div>"))
-			$("section.places article#" + data[i].id + "").append($("<div class='information'></div>"))
-			$("section.places article#" + data[i].id + " div.information").append($("<div class='max_guest'> Guest " + data[i].max_guest + (data[i].max_guest != 1 ? 's' : '') + "</div>"))
-			$("section.places article#" + data[i].id + " div.information").append($("<div class='number_rooms'> " + data[i].number_rooms + " Bedroom" + (data[i].max_guest != 1 ? 's' : '') + "</div>"))
-			$("section.places article#" + data[i].id + " div.information").append($("<div class='number_bathrooms'>" + data[i].number_bathrooms + " Bathroom" + (data[i].number_bathrooms != 1 ? 's' : '') + "</div>"))
-			$("section.places article#" + data[i].id + "").append($("<div class='user'></div>"))
-			$.get("http://localhost:5001/api/v1/users/" + data[i].user_id,
-			function(data_user)
-			{
-			    $("section.places article#" + data[i].id + " div.user").append($("<b>Owner: </b>"))
-			    $("section.places article#" + data[i].id + " div.user").append((data_user.first_name + " " + data_user.last_name))
-			});
-			$("section.places article#" + data[i].id + "").append($("<div class='description'></div>"))
-			$("section.places article#" + data[i].id + " div.description").append(data[i].description)
-		    }
-		},
-		dataType: 'json'
-	      });
+            for (let i = 0; i < data.length; i++) {
+                $("section.places").append($("<article id=" + data[i].id + "></article>"))
+                $("section.places article#" + data[i].id).append($("<div class='title_box'></div>"))
+                $("section.places article#" + data[i].id + " div.title_box").append($("<h2>" + data[i].name + "</h2>"))
+                $("section.places article#" + data[i].id + " div.title_box").append($("<div class='price_by_night'>$" + data[i].price_by_night + "</div>"))
+                $("section.places article#" + data[i].id + "").append($("<div class='information'></div>"))
+                $("section.places article#" + data[i].id + " div.information").append($("<div class='max_guest'> Guest " + data[i].max_guest + (data[i].max_guest != 1 ? 's' : '') + "</div>"))
+                $("section.places article#" + data[i].id + " div.information").append($("<div class='number_rooms'> " + data[i].number_rooms + " Bedroom" + (data[i].max_guest != 1 ? 's' : '') + "</div>"))
+                $("section.places article#" + data[i].id + " div.information").append($("<div class='number_bathrooms'>" + data[i].number_bathrooms + " Bathroom" + (data[i].number_bathrooms != 1 ? 's' : '') + "</div>"))
+                $("section.places article#" + data[i].id + "").append($("<div class='user'></div>"))
+                $.get("http://localhost:5001/api/v1/users/" + data[i].user_id,
+                    function (data_user) {
+                        $("section.places article#" + data[i].id + " div.user").append($("<b>Owner: </b>"))
+                        $("section.places article#" + data[i].id + " div.user").append((data_user.first_name + " " + data_user.last_name))
+                    });
+                $("section.places article#" + data[i].id + "").append($("<div class='description'></div>"))
+                $("section.places article#" + data[i].id + " div.description").append(data[i].description)
+            }
+        },
+        dataType: 'json'
+    });
 }
